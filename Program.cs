b18e9d6be075ca9387f473998a028b11ef91c2a0
Regina Shakirova <reginafar22@rambler.ru>
2022-11-20 20:32:27 +0400

    Добавила файл readme

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿Console.WriteLine("Введите размер массива:");
 int size = Convert.ToInt32(Console.ReadLine());
 string [] array = new string [size];
+string [] result = new string[size];
 Console.WriteLine("Введите элементы массива:");
 for (int i = 0; i < size; i++)
 {
     array[i] = Console.ReadLine();
 }
 Console.Write("[" + string.Join (" ",array)+"]");
-string [] result = new string[size];
 for (int i = 0; i < size; i++)
 {
     if (array[i].Length <= 3)
